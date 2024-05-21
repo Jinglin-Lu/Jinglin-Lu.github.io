6abc1b33df650a54bb35f59451afee0a2fa055e5
Jinglin-Lu <157343786+Jinglin-Lu@users.noreply.github.com>
2024-05-20 19:48:08 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,10 +1,10 @@
 var playlist = [
-    { title: "The Beatles - Yellow Submarine", src: "audio/The Beatles - Yellow Submarine.mp3", albumArt: "images/The Beatles - Yellow Submarine.jpg" },
-    { title: "Leehom Wang - 唯一", src: "audio/Leehom Wang - 唯一.mp3", albumArt: "images/Leehom Wang - 唯一.jpg" },
-    { title: "Elvis Presley - Can't Help Falling In Love (Official Audio)", src: "audio/Elvis Presley - Can't Help Falling In Love (Official Audio).mp3", albumArt: "images/Elvis Presley - Can't Help Falling In Love (Official Audio).png" },
-    { title: "Bob Dylan - Don't Think Twice, It's All Right (Official Audio)", src: "audio/Bob Dylan - Don't Think Twice, It's All Right (Official Audio).mp3", albumArt: "images/Bob Dylan - Don't Think Twice, It's All Right (Official Audio).jpeg" },
-    { title: "Billy Joel - Piano Man (Official HD Video)", src: "audio/Billy Joel - Piano Man (Official HD Video).mp3", albumArt: "images/Billy Joel - Piano Man (Official HD Video).jpeg" },
-    { title: "Abba - Dancing Queen (Official Music Video Remastered)", src: "audio/Abba - Dancing Queen (Official Music Video Remastered).mp3", albumArt: "images/Abba - Dancing Queen (Official Music Video Remastered).jpeg" }
+    { title: "The Beatles - Yellow Submarine", src: "../audio/The Beatles - Yellow Submarine.mp3", albumArt: "../images/The Beatles - Yellow Submarine.jpg" },
+    { title: "Leehom Wang - 唯一", src: "../audio/Leehom Wang - 唯一.mp3", albumArt: "../images/Leehom Wang - 唯一.jpg" },
+    { title: "Elvis Presley - Can't Help Falling In Love (Official Audio)", src: "../audio/Elvis Presley - Can't Help Falling In Love (Official Audio).mp3", albumArt: "../images/Elvis Presley - Can't Help Falling In Love (Official Audio).png" },
+    { title: "Bob Dylan - Don't Think Twice, It's All Right (Official Audio)", src: "../audio/Bob Dylan - Don't Think Twice, It's All Right (Official Audio).mp3", albumArt: "../images/Bob Dylan - Don't Think Twice, It's All Right (Official Audio).jpeg" },
+    { title: "Billy Joel - Piano Man (Official HD Video)", src: "../audio/Billy Joel - Piano Man (Official HD Video).mp3", albumArt: "../images/Billy Joel - Piano Man (Official HD Video).jpeg" },
+    { title: "Abba - Dancing Queen (Official Music Video Remastered)", src: "../audio/Abba - Dancing Queen (Official Music Video Remastered).mp3", albumArt: "../images/Abba - Dancing Queen (Official Music Video Remastered).jpeg" }
 ];
 
 var currentTrack = 0;
